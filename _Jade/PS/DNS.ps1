b7c84297b2581679e36854e4024b0ed32affa995
jugarapjade <jillustrisimo32@gmail.com>
2024-08-11 16:53:14 +0800

    Add files via upload

---
+++ _Jade/PS/DNS.ps1
@@ -0,0 +1,4 @@
+﻿#Add-DnsServerPrimaryZone -Name "CCNA21.com" -ZoneFile "CCNA21.com.dns"
+#Add-DnsServerPrimaryZone -Name "cisco21.com" -ZoneFile "cisco21.com.dns"
+#Add-DnsServerPrimaryZone -Name "jade.com" -ZoneFile "jade.com.dns"
+Add-DnsServerPrimaryZone -Name "NetEng21.com" -ZoneFile "NetEng21.com.dns"
